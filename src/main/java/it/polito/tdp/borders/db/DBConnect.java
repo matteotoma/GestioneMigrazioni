03328c30fc689d9b2533948f23ea64d071275ece
Matteo <Matteo@Matteo-HP.homenet.telecomitalia.it>
2020-05-20 09:34:57 +0200

    Esercizio completo

--- src/main/java/it/polito/tdp/borders/db/DBConnect.java
+++ src/main/java/it/polito/tdp/borders/db/DBConnect.java
@@ -17,7 +17,7 @@ public class DBConnect {
 			HikariConfig config = new HikariConfig();
 			config.setJdbcUrl(jdbcURL);
 			config.setUsername("root");
-			config.setPassword("rootroot");
+			config.setPassword("27dicembre1998");
 			
 			//configurazione mysql
 			config.addDataSourceProperty("cachePrepStmts", "true");
